c2508da2e4b8f48894acdcafd9d3361ae91224a0
sighakolli susmitha <73429559+sighakollisusmitha@users.noreply.github.com>
2021-10-21 09:39:12 +0530

    Add files via upload

---
+++ transfermoneynav.php
@@ -0,0 +1,14 @@
+<header>
+        <div class="logoimg">
+            <img src="static/logo.png" alt="logo">
+        </div>
+        <div class="logo">Sparks Bank</div>
+        <div class="navigation">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="createuser.php">Create User</a></li>
+                <li><a class="active" href="transfermoney.php">Transfer Money</a></li>
+                <li><a href="transactionhistory.php">Transaction History</a></li>
+            </ul>
+        </div>
+</header>
